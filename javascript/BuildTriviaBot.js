01c204df897fe11e186a68ee6b337ebd38c15a77
BeSingularTeam <besingulardemo@gmail.com>
2025-08-10 22:46:47 +0530

    commit

---
+++ javascript/BuildTriviaBot.js
@@ -0,0 +1,21 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName="Codey";
+let botLocation="London";
+
+let favoriteLanguage= "Javascript";
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+// Step 4: Favorite programming language
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+let codingFact = "Fun fact about your bot's favorite coding language "+favoriteLanguage;
+console.log(codingFact);
+// Step 6: Second fun fact
+codingFact = "Another fun fact: " + favoriteLanguage + " can be used for both frontend and backend development.";
+console.log(codingFact);
+
+// Step 7: Third fun fact
+codingFact = "Final fact: " + favoriteLanguage + " is one of the most popular languages in the world.";
+console.log(codingFact);
+
+// Step 8: Farewell
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
