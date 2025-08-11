0f590a14d21b7185ecdf380355adab2eb7258882
BeSingularTeam <besingulardemo@gmail.com>
2025-08-11 15:45:23 +0530

    commit

---
+++ javascript/Variables And Strings/BuiltSentenceMaker.js
@@ -0,0 +1,30 @@
+let adjective = "happy";
+let noun = "cat";
+let verb = "dance";
+let place = "park";
+let adjective2 = "fluffy";
+let noun2 = "ball";
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + 
+                 " who loved to eat " + noun2 + ". " +
+                 "The " + noun + " lived in a " + place + 
+                 " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: "+ firstStory);
+
+adjective = "curious";
+noun = "robot";
+verb = "explore";
+place = "library";
+adjective2 = "ancient";
+noun2 = "book";
+let secondStory ="Once upon a time, there was a(n) " + adjective + " " + noun + 
+                 " who loved to eat " + noun2 + ". " +
+                 "The " + noun + " lived in a " + place + 
+                 " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
+
+
+
+
+
+
