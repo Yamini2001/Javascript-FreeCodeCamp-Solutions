2e52e7b1c331f3be783b73a06bc1440487d7c2d4
BeSingularTeam <besingulardemo@gmail.com>
2025-08-12 16:13:14 +0530

    converter

---
+++ javascript/Booleans And Numbers/CelsiusToFarenheitConverter.js
@@ -0,0 +1,10 @@
+function convertCtoF(Celsius) {
+  return Celsius * 9 / 5 + 32;
+}
+
+console.log(convertCtoF(0));    // 32
+console.log(convertCtoF(-30));  // -22
+console.log(convertCtoF(-10));  // 14
+console.log(convertCtoF(0));    // 32
+console.log(convertCtoF(20));   // 68
+console.log(convertCtoF(30));   // 86
